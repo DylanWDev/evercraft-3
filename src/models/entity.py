7c1eb49a8033b4c12529ca9f41cf218e88d88cf8
DylanWDev <dylanwyrick11@icloud.com>
2023-10-25 19:43:24 +0000

    passed armor class test

--- src/models/entity.py
+++ src/models/entity.py
@@ -1,6 +1,7 @@
 # class template to import into iterations
 class Entity:
-    def __init__(self, name, alignment):
+    def __init__(self, name, alignment, armor_class):
         self.name = name
         # self.character_class = character_class
         self.alignment = alignment
+        self.armor_class = 10
--- tests/test_iteration_1/test_iteration_1.py
+++ tests/test_iteration_1/test_iteration_1.py
@@ -23,10 +23,13 @@ def test_create_character():
     # alignments are good, evil, neutral
 
 def test_create_alignment():
-    entity1 = Entity("bob", "good")
+    entity1 = Entity("bob", "good", 16)
     assert entity1.alignment is "good"
 
 # as a combatant i want to have armor class and hit points so that i can resist attacks from my enemies
     # has an armor class that defaults to 10
     # has 5 hit points by default
 
+def test_create_armor_class():
+    entity1 = Entity("bob", "good", 16)
+    assert entity1.armor_class is 16
