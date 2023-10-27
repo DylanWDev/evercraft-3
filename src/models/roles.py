fc20d905259e407033e8d6f492d06a42fbb06710
DylanWDev <dylanwyrick11@icloud.com>
2023-10-27 13:03:41 +0000

    save point

--- src/models/roles.py
+++ src/models/roles.py
@@ -2,20 +2,20 @@
 # As a player I want to play a Fighter so that I can kick 
 # butt and take names 
 
-""" 
-attacks roll is increased by 1 for every level instead 
-of every other level has 10 hit points 
-per level instead of 5
-"""
+# """ 
+# attacks roll is increased by 1 for every level instead 
+# of every other level has 10 hit points 
+# per level instead of 5
+# """
 
 class Fighter:
-    def __init__(self,level):
-        self.level = level
+    def __init__(self, hp):
+        self.hp = hp
 
     # function so that attacks roll is increased by 1 
     #  level instead of default every other level
-    def attack_roll(self):
-        return self.level
+    # def attack_roll(self):
+    #     return self.level
         # attack advantage function
         # default (if not a fighter) - every other level-up - increases attack level by 1
         # a fighter - every level-up - increases attack level by 1
@@ -26,8 +26,8 @@ class Fighter:
     # function so that hp increases by 10 hp rather
     # than default 5 hp each level
 
-    def hit_points(self):
-        return self.level 
+    # def hit_points(self):
+    #     return self.level 
         # hp (hit points) advantage function
         # default (if not a fighter) - every level-up - increases hp level by 5
         # a fighter - every level-up - increases hp level by 10
--- tests/test_iteration_2/test_iteration_2.py
+++ tests/test_iteration_2/test_iteration_2.py
@@ -1,24 +1,30 @@
-# Feature: Characters Have Classes
-# As a player I want a character to have a class that customizes its capabilities so 
-# that I can play more interesting characters
-
+from src.models.roles import Fighter
 from src.models.entity import Entity
-from src.models.
+
 import pytest
 
-# LETS TRY SAMPLE 1:
-# As a player I want to play a Fighter so that I can kick 
-# butt and take names 
+# Feature: Characters Have Classes
+# As a player I want a character to have a class that customizes its capabilities so
+# that I can play more interesting chaactersr
 
-# attacks roll is increased by 1 for 
-# every level instead of every other level has 10 hit points 
+
+# LETS TRY SAMPLE 1:
+# As a player I want to play a Fighter so that I can kick
+# butt and take names
+
+# attacks roll is increased by 1 for
+# every level instead of every other level has 10 hit points
 # per level instead of 5
 
 # character ROLE - meaning "fighter", "mage", etc...
 # make a class for character roles
 # need to make classes that can be passed to any character we create - aka entity
 # each class should have its own pre-determined attributes
-    # this is so we can later compare these to each other to test...
+# this is so we can later compare these to each other to test...
+
 
 def test_fighter_hp_advantage():
-    fighter = 
+    # giving fighter its default level 10 and default 5 hp
+    fighter = Fighter(10)
+    # if the fighter levels up to 2 then its default should be plus 10
+    assert fighter.hp is 10
